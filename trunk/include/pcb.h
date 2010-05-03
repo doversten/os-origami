dfb9f8bffd4b4bb18e32ebb6923fc862ae56546e
lejums <lejums@1122e86d-ba6d-83d1-f69a-24096d9ac2e3>
2010-05-03 11:22:07 +0000

    pcb och queue funktioner

--- trunk/include/pcb.h
+++ trunk/include/pcb.h
@@ -4,20 +4,24 @@
 #include "types.h"
 #include "registers.h"
 
+#define NUMBER_OF_PROCESSES 16
+#define NULL 0
+
 typedef struct pcb_t
 {
-  uint32_t pid;
-  uint32_t priority;
-  union {
-      uint8_t value;
-      struct {
-        uint8_t ready : 1;  //Ready bit, 1 if process is ready to be executed 0 if it's blocked or pending some event.
-        uint8_t empty : 1;  //Valid bit, 0 if PCB contains an active process, 1 otherwise.
-      } field;
-  } status;
-  registers_t regs;
-  struct pcb_t *next;
-  struct pcb_t *prev;
+	uint32_t pid;
+	uint32_t priority;			//The lower the value is the higher the priority is, priority >= 0	
+	union {
+			uint8_t value;
+			struct {
+				uint8_t ready : 1;	//Ready bit, 1 if process is ready to be executed 0 if it's blocked or pending some event.
+				uint8_t empty : 1;	//Valid bit, 0 if PCB contains an active process, 1 otherwise.
+			} field;
+	} status;
+	registers_t regs;
+	struct pcb_t *next;
+	struct pcb_t *prev;
+	stack_t *stack;
 
 } pcb_t;
 
